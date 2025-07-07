10fc52e12e4128d1c39d0fe45829d78fb7187af9
AJINKYA <ajinkyacodes@gmail.com>
2025-07-07 20:00:26 +0530

    Created a Spring Boot REST API that returns a list

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -16,5 +19,16 @@ public class StudentController {
                 "Doe"
         );
         return student;
+    }
+
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "John", "Doe"));
+        students.add(new Student(2, "Adam", "Smith"));
+        students.add(new Student(3, "Robin", "Sharma"));
+        students.add(new Student(4, "Kabir", "Shikhawat"));
+        return students;
     }
 }
