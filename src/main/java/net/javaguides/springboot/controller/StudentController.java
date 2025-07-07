e06c87398e6ca78832a65e702e67166303569d65
AJINKYA <ajinkyacodes@gmail.com>
2025-07-07 20:11:48 +0530

    Created PathVariable for url with multiple path

--- src/main/java/net/javaguides/springboot/controller/StudentController.java
+++ src/main/java/net/javaguides/springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,22 +14,29 @@ public class StudentController {
     // http://localhost:8080/student
     @GetMapping("student")
     public Student getStudent() {
-        Student student = new Student(
-                1,
-                "John",
-                "Doe"
-        );
+        Student student = new Student(1, "John", "Doe");
         return student;
     }
 
     // http://localhost:8080/students
     @GetMapping("students")
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         List<Student> students = new ArrayList<>();
         students.add(new Student(1, "John", "Doe"));
         students.add(new Student(2, "Adam", "Smith"));
         students.add(new Student(3, "Robin", "Sharma"));
         students.add(new Student(4, "Kabir", "Shikhawat"));
         return students;
+    }
+
+    // Spring Boot REST API with Path Variable
+    // {id} - URI template variable
+    // http://localhost:8080/students/1/john/doe
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable(
+            @PathVariable("id") int studentId,
+            @PathVariable("first-name") String firstName,
+            @PathVariable("last-name") String lastName) {
+        return new Student(studentId, firstName, lastName);
     }
 }
